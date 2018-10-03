92a85378ed32096891c8582072f6f0ea394767fd
nbecavin <binouze33>
2018-10-03 22:49:40 +0200

    Remove old/unused stuffs

--- SandBoxDroid/gen/nbecavin/sandboxdroid/engine/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package nbecavin.sandboxdroid.engine;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- SandBoxEditor/Form1.cpp
+++
@@ -1,9 +0,0 @@
-#include "stdafx.h"
-#include "Form1.h"
-
-using namespace SandBoxEditor;
-
-void Form1::LoadConfigIni()
-{
-	
-}
