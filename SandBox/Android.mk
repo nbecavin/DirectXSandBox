b1ceb65e222971fa1a6c92beca78537ab0770cb0
nbecavin <binouze33>
2018-10-04 21:43:00 +0200

    Remove Android PTF and migrate to VS2017 & WinSDK 17xxx

--- SandBox/Android.mk
+++
@@ -1,8 +0,0 @@
-
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-LOCAL_MODULE := SandBoxNativeLib
-
-LOCAL_SRC_FILES := nv_math.cpp nv_matrix.cpp nv_quat.cpp
-
-include $(BUILD_STATIC_LIBRARY)
